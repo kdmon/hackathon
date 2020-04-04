4b9e9d3186ddab482c07c7c487d18d16584525e7
kdmon <kdmon@users.noreply.github.com>
2020-04-04 22:36:04 +0200

    Update file.

---
+++ views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Gruppsamtal</h1>
+    <q-btn
+      unelevated
+      no-caps
+      label="Logga in med BankId"
+      color="green-6" 
+      text-color="black"
+      to="/waitingroom">
+    </q-btn>
+  </div>
+</template>
