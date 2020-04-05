02ac55d1b312273d4904ef09e390c45f28ece5db
kdmon <kdmon@users.noreply.github.com>
2020-04-05 05:11:41 +0200

    Update file.

--- views/waitingroom.vue
+++ views/waitingroom.vue
@@ -1,48 +1,82 @@
 <template>
   <div class="row">
-    <div class="col-xl-1 col-lg-2 col-md-3 col-sm-12 bg-black text-white">
-      <h1 style="margin: 0.25em;">Väntrum</h1>
+    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-12 bg-black text-white">
+      <!--<h3 style="margin: 0.25em; font-size: 2em">Inställningar</h3>-->
+
+      <br>
       
       <video id="localVideo"></video>
+      
+      <div v-if="connected" id="controls" style="display: flex; align-items: center; justify-content: center; margin-top: -7em;">
+        <q-btn v-if="micOff" @click="micOff = false" fab color="blue" class="q-ma-xs" icon="mic_off">
+          <q-tooltip>Sätt på mikrofonen</q-tooltip>
+        </q-btn>
         
-      <q-btn label="Avboka" @click="cancel('hej')"></q-btn> 
+        <q-btn v-if="!micOff" @click="micOff = true" fab color="white" class="q-ma-xs" outline icon="mic">
+          <q-tooltip>Stäng av mikrofonen</q-tooltip>
+        </q-btn>
+
+        <q-btn fab v-if="shareVideo" @click="shareVideo = false" color="white" class="q-ma-xs" outline icon="videocam">
+          <q-tooltip>Sluta dela video</q-tooltip>
+        </q-btn>
+        
+        <q-btn fab v-if="!shareVideo" @click="shareVideo = true" color="blue" class="q-ma-xs" icon="videocam_off">
+          <q-tooltip>Börja dela video</q-tooltip>
+        </q-btn>
+        
+        <q-btn fab v-if="!screenSharing" @click="shareScreen()" color="blue" class="q-ma-xs" icon="stop_screen_share">
+          <q-tooltip>Börja dela skärm<br>(Kräver Firefox)</q-tooltip>
+        </q-btn>
+        
+        <q-btn fab v-if="screenSharing" @click="shareScreen(true)" color="white" class="q-ma-xs" outline icon="screen_share">
+          <q-tooltip>Sluta dela skärm</q-tooltip>
+        </q-btn>
+        
+        <q-btn fab @click="closeConnection" color="red" class="q-ma-xs" icon="call_end">
+          <q-tooltip>Lämna samtalet</q-tooltip>
+        </q-btn>
       
-      <q-btn label="dela skärm" @click="shareScreen"></q-btn>
-      
-      <q-btn label="Lägg på" @click="closeConnection"></q-btn>
-      
+      </div>
+
     </div>
-    <div class="col-xl-11 col-lg-10 col-md-9 col-sm-12" id="splash">
+    <div class="col-xl-10 col-lg-9 col-md-9 col-sm-12" id="splash">
       
-      <q-btn fab icon="arrow_back" color="black" class="q-ma-lg" @click="closeConnection"></q-btn>
+      <div v-if="!connected">
+        <q-btn label="Avsluta och lämna väntrummet" no-caps icon="arrow_back" unelevated text-color="white" class="q-ma-lg" @click="leaveRoom"></q-btn>
+        
+        <!--<h1 class="text-white q-ma-lg">Mina samtal</h1>-->
+        
+        <q-card class="shadow-10 text-white q-ma-lg" style="border: 1px solid #555; background: #0000004f; max-width: 450px; ccursor: pointer;" @oonclick="startMeeting">
+          <q-card-section>
+            <div style="width:50px; display: inline-block; vertical-align: top;">
+              <img style="border-radius: 100%; width: 48px" src="https://randomuser.me/api/portraits/men/9.jpg">
+              <q-tooltip>Anders Frisk<br>Leg. psykolog</q-tooltip>
+            </div>
+            <div style="display: inline-block">
+              <h2 class="q-ma-none">Samtalsterapi</h2>
+              <h3 class="q-my-none" sstyle="margin-left:2em">
+                Idag 10:00-10:45
+              </h3>
+              <br>
+              <p v-if="connected">
+                Väntar på att samtalet ska starta ... <q-spinner></q-spinner>
+              <!--<q-btn fab icon="call" color="lime-7"></q-btn>-->
+              </p>
+              <p v-else>Du har inte anslutit till samtalet ännu.</p>
+            </div>
+          </q-card-section>
+          <q-separator color="grey-8"></q-separator>
+          <q-card-actions align="between">
+            <q-btn color="white" outline no-caps text-color="white" label="Lämna återbud" @click="cancel('hej')"></q-btn> 
+            <q-btn v-if="readyToCall" color="green-6" no-caps text-color="white" icon="call" label="Anslut till samtalet" unelevated @click="joinRoom('samtalsterapi')"></q-btn> 
+            <!-- <q-btn v-else color="green-3" no-caps text-color="black" label="Ansluter..." unelevated @click="leaveRoom('samtalsterapi')"></q-btn> -->
+          </q-card-actions>
+        </q-card>
+      </div>
       
-      <q-card class="shadow-10 text-white q-ml-lg" style="border: 1px solid #555; background: #0000004f; max-width: 450px; ccursor: pointer;" @oonclick="startMeeting">
-        <q-card-section>
-          <div style="width:50px; display: inline-block; vertical-align: top;">
-            <img style="border-radius: 100%; width: 48px" src="https://randomuser.me/api/portraits/men/9.jpg">
-          </div>
-          <div style="display: inline-block">
-            <h2 class="q-ma-none">Samtalsterapi</h2>
-            <h3 class="q-my-none" sstyle="margin-left:2em">
-              10:00-10:45
-            </h3>
-            <br>
-            <p v-if="connecting">
-              Väntar på att Morris Stewart ska starta mötet...
-            <!--<q-btn fab icon="call" color="lime-7"></q-btn>-->
-            </p>
-            <p v-else>Du har inte anslutit till mötet ännu.</p>
-          </div>
-        </q-card-section>
-        <q-separator color="grey-8"></q-separator>
-        <q-card-actions align="between">
-          <q-btn color="white" outline no-caps text-color="white" label="Lämna återbud" @click="cancel('hej')"></q-btn> 
-          <q-btn v-if="!connecting" color="white" no-caps text-color="black" label="Anslut till mötet" unelevated @click="connecting = true"></q-btn> 
-          <q-btn v-else color="green-3" no-caps text-color="black" label="Ansluter..." unelevated @click="joinRoom()">
-          </q-btn> 
-        </q-card-actions>
-      </q-card>
-      <div id="remoteVideos"></div>
+      <div v-else>
+        <div id="remoteVideos" :class="'feeds-' + feeds"></div>
+      </div>
 
       <!--
       <q-btn fab icon="call" color="lime-7"></q-btn>
@@ -50,7 +84,7 @@
       
     </div>
     
-    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-page-sticky position="bottom-right" :offset="[18, 18]" v-if="!connected"> 
       <h2 style="font-weight: 100; margin: 0" class="text-white">{{ currentTime }}</h2>
       <h3 style="font-weight: 100; margin: 0" class="text-white">{{ currentDate }}</h3>
     </q-page-sticky>
@@ -61,10 +95,14 @@
   module.exports = {
     data: function () {
       return {
-        connecting: false,
+        micOff: false,
+        shareVideo: true,
+        screenSharing: false,
+        connected: false,
         currentTime: '',
         currentDate: '',
-        readyToCall: false
+        readyToCall: false,
+        feeds: 0
       }
     },
     mounted: async function () {
@@ -72,54 +110,63 @@
       setTimeout(() => this.startMeeting(), 4000)
       setInterval(() => this.currentDate =  new Date().toLocaleDateString(), 1000)
       setInterval(() => this.currentTime =  new Date().toLocaleTimeString(), 1000)
-      Vue.prototype.$webrtc = await new SimpleWebRTC({
-        url: "https://webappeditor.com:8085",
-        socketio: {'force new connection': true},
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideos',
-        autoRequestMedia: true,
-        media: {
-          video: {
-            mandatory: {
-              //chromeMediaSource: 'desktop',
-			        //chromeMediaSourceId: 'screen',
-              maxFrameRate: 15,
-              maxWidth: 320,
-              maxHeight: 240
-            }
-          },
-          audio: true
-        }
-      });
-      
-      this.$webrtc.on('readyToCall', () => {
-        this.readyToCall = true
-      })
+      setInterval(() => this.checkFeeds(), 500)
+      this.initCamera()
     },
     methods: {
       closeConnection : function (room) {
-        window.location = 'https://webappeditor.com/kdmon/hackathon/master/index.html#/'
+        //window.location = 'https://webappeditor.com/kdmon/hackathon/master/index.html#/waitingroom'
         location.reload()
+      },
+      checkFeeds : function () {
+        let e = document.getElementById('remoteVideos')
+        if (e) this.feeds = e.childElementCount
       },
       startMeeting : function () {
         //alert ("Mötet startar")
       },
-      joinRoom : function (room) {
-        connecting = false
-        if (this.readyToCall) {
-          setTimeout(() => {
-            this.$webrtc.joinRoom(room)
-            $router.push({ name: 'chat', params: { room: 'samtalsterapi' }})
-          }, 4000)
-        }
+      leaveRoom : function () {
+        window.location = 'https://webappeditor.com/kdmon/hackathon/master/index.html'
       },
-      shareScreen : function () {
-        if (this.$webrtc.getLocalScreen()) {
-          this.$webrtc.stopScreenShare()
-        } else {
-        this.$webrtc.shareScreen (function (e) {
-          console.log(e)
+      initCamera : function () {
+        Vue.prototype.$webrtc = new SimpleWebRTC({
+          url: "https://webappeditor.com:8085",
+          socketio: {'force new connection': true},
+          localVideoEl: 'localVideo',
+          remoteVideosEl: 'remoteVideos',
+          autoRequestMedia: true,
+          media: {
+            video: {
+              mandatory: {
+                //chromeMediaSource: 'desktop',
+  			        //chromeMediaSourceId: 'screen',
+                maxFrameRate: 30
+                //maxWidth: 320,
+                //maxHeight: 240
+              }
+            },
+            audio: true
+          }
+        });
+        
+        this.$webrtc.on('readyToCall', () => {
+          this.readyToCall = true
         })
+  
+      },
+      joinRoom :  function (room) {
+        this.connected = true
+        this.$webrtc.joinRoom(room)
+      },
+      shareScreen : function (stop) {
+        if (this.$webrtc.getLocalScreen() || stop) {
+          this.$webrtc.stopScreenShare()
+          this.screenSharing = false
+        } else {
+          this.$webrtc.shareScreen(function (e) {
+            console.log(e)
+          })
+          this.screenSharing = true
         }
       }
     }
@@ -138,5 +185,62 @@
     border: 1em solid black;
     border-radius: 2em;
   }
+  
+  #remoteVideos {
+    display: flex;
+    width: 100%;
+    height: calc(100vh - 50px);
+    background: #aaa;
+  }
+  
+  .feeds-1 video {
+    width: 100%;
+    height: 100%;
+  }
+  
+  .feeds-2 video {
+    width: 50%;
+    height: 100%;
+  }
+  
+  .feeds-3 video {
+    width: 33%;
+    height: 100%;
+  }
+  
+  .feeds-4 video {
+    width: 50%;
+    height: 50%;
+  }
+  
+  .feeds-5 video {
+    width: 33%;
+    height: 50%;
+  }
+  
+  .feeds-6 video {
+    width: 33%;
+    height: 50%;
+  }
+  
+  .feeds-7 video {
+    width: 33%;
+    height: 33%;
+  }
+
+  .feeds-8 video {
+    width: 33%;
+    height: 33%;
+  }
+
+  .feeds-9 video {
+    width: 33%;
+    height: 33%;
+  }
+  
+  #controls {
+    opacity: 0.8;
+  }
+
 </style>
   
